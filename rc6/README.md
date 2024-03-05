c4ad66baa30944a26b5944d46cf2d7cd3e06c0cb
Brian Yarr <brian.yarr@gmail.com>
2024-03-05 08:07:22 +0000

    Initial version of RC6
    Supports a single hardcoded parameter set w=32, r=20, b=16. Only encryption implemented.

---
+++ rc6/README.md
@@ -0,0 +1,3 @@
+# [RustCrypto]: RC6 Cipher
+
+Pure Rust implementation of the [RC6] block cipher.
---
+++ rc6/src/lib.rs
@@ -0,0 +1,141 @@
+#![no_std]
+
+use cipher::array::Array;
+use cipher::consts::{U1, U16, U44};
+use cipher::inout::InOut;
+
+use cipher::{
+    Block, BlockBackend, BlockCipher, BlockCipherEncrypt, BlockClosure, BlockSizeUser,
+    Key, KeyInit, KeySizeUser, ParBlocksSizeUser,
+};
+use core::cmp::max;
+
+use core::ops::{BitAnd, BitXor};
+
+// This should be parameterised but hard code for now
+// W - word size (bits) - 32
+// R - number of rounds - 20
+// B - key length in bytes - 16
+// Start with rounds = 20, w =32
+
+pub struct RC6 {
+    key: Array<u8, U16>,
+}
+
+impl BlockCipher for RC6 {}
+
+impl KeySizeUser for RC6 {
+    type KeySize = U16;
+}
+
+impl BlockSizeUser for RC6 {
+    type BlockSize = U16;
+}
+impl KeyInit for RC6 {
+    #[inline]
+    fn new(key: &Key<Self>) -> Self {
+        key_expansion(key);
+        Self { key: *key }
+    }
+}
+
+impl BlockCipherEncrypt for RC6 {
+    fn encrypt_with_backend(&self, f: impl BlockClosure<BlockSize = Self::BlockSize>) {
+        f.call(&mut RC6EncBackend {
+            expanded_key: key_expansion(&self.key),
+        })
+    }
+}
+
+struct RC6EncBackend {
+    expanded_key: Array<u32, U44>,
+}
+
+impl ParBlocksSizeUser for RC6EncBackend {
+    type ParBlocksSize = U1;
+}
+
+impl BlockSizeUser for RC6EncBackend {
+    type BlockSize = U16;
+}
+
+impl BlockBackend for RC6EncBackend {
+    fn proc_block(&mut self, mut block: InOut<'_, '_, Block<Self>>) {
+        let mut a = u32::from_le_bytes(block.get_in()[..4].try_into().unwrap());
+        let mut b = u32::from_le_bytes(block.get_in()[4..8].try_into().unwrap());
+        let mut c = u32::from_le_bytes(block.get_in()[8..12].try_into().unwrap());
+        let mut d = u32::from_le_bytes(block.get_in()[12..16].try_into().unwrap());
+
+        b = b.wrapping_add(self.expanded_key[0]);
+        d = d.wrapping_add(self.expanded_key[1]);
+        for i in 1..=20 {
+            let t = b
+                .wrapping_mul(b.wrapping_mul(2).wrapping_add(1))
+                .rotate_left(5);
+            let u = d
+                .wrapping_mul(d.wrapping_mul(2).wrapping_add(1))
+                .rotate_left(5);
+
+            a = (a.bitxor(t))
+                .rotate_left(u)
+                .wrapping_add(self.expanded_key[2 * i]);
+            c = (c.bitxor(u))
+                .rotate_left(t)
+                .wrapping_add(self.expanded_key[2 * i + 1]);
+
+            let ta = a;
+            let tb = b;
+            let tc = c;
+            let td = d;
+
+            a = tb;
+            b = tc;
+            c = td;
+            d = ta;
+        }
+        a = a.wrapping_add(self.expanded_key[42]);
+        c = c.wrapping_add(self.expanded_key[43]);
+
+        block.get_out()[0..4].copy_from_slice(&a.to_le_bytes());
+        block.get_out()[4..8].copy_from_slice(&b.to_le_bytes());
+        block.get_out()[8..12].copy_from_slice(&c.to_le_bytes());
+        block.get_out()[12..16].copy_from_slice(&d.to_le_bytes());
+    }
+}
+
+fn key_expansion(key: &Array<u8, U16>) -> Array<u32, U44> {
+    let _r = 20;
+    let c = 4;
+    let b = 16;
+    let w = 32;
+    let u = w / 8;
+
+    let mut l = [0u32; 4];
+    for i in (0..(b - 1)).rev() {
+        l[i / u] = (l[i / u] << 8) | (key[i] as u32);
+    }
+
+    let mut s: Array<u32, U44> = Array::from_fn(|_| 0u32);
+    s[0] = 0xB7E15163;
+
+    for i in 1..44 {
+        s[i] = s[i - 1].wrapping_add(0x9E3779B9);
+    }
+
+    let mut a = 0;
+    let mut b = 0;
+    let mut i = 0;
+    let mut j = 0;
+
+    let v = 3 * max(c, 44);
+    for _s in 1..=v {
+        a = s[i].wrapping_add(a).wrapping_add(b).rotate_left(3);
+        s[i] = a;
+        b = (l[j].wrapping_add(a).wrapping_add(b)).rotate_left(a.wrapping_add(b).bitand(0b11111));
+        l[j] = b;
+        i = (i + 1) % 44;
+        j = (j + 1) % c;
+    }
+
+    s
+}
