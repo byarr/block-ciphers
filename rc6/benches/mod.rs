5c6eb1eb5d3907d5d65ce4a195a0b7962acf137a
Brian Yarr <brian.yarr@gmail.com>
2024-03-08 08:52:27 +0000

    Update benchmarks to support different user supplied key sizes

--- rc6/benches/mod.rs
+++ rc6/benches/mod.rs
@@ -3,16 +3,30 @@ extern crate test;
 
 use cipher::block_decryptor_bench;
 use cipher::block_encryptor_bench;
+use cipher::consts::{U16, U32};
 use rc6::RC6;
 
 block_encryptor_bench!(
-    Key: RC6,
-    rc6_encrypt_block,
-    rc6_encrypt_blocks,
+    Key: RC6<U16>,
+    rc6_32_20_16_encrypt_block,
+    rc6_32_20_16_encrypt_blocks,
 );
 
 block_decryptor_bench!(
-    Key: RC6,
-    rc6_decrypt_block,
-    rc6_decrypt_blocks,
+    Key: RC6<U16>,
+    rc6_32_20_16_decrypt_block,
+    rc6_32_20_16_decrypt_blocks,
+);
+
+
+block_encryptor_bench!(
+    Key: RC6<U32>,
+    rc6_32_20_32_encrypt_block,
+    rc6_32_20_32_encrypt_blocks,
+);
+
+block_decryptor_bench!(
+    Key: RC6<U32>,
+    rc6_32_20_32_decrypt_block,
+    rc6_32_20_32_decrypt_blocks,
 );
