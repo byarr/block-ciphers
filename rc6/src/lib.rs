14022d8c7a00f419a7fea90e1e6a70a25d5ec972
Brian Yarr <brian.yarr@gmail.com>
2024-03-08 08:41:50 +0000

    Support different user supplied key sizes

--- rc6/src/lib.rs
+++ rc6/src/lib.rs
@@ -1,12 +1,21 @@
 #![no_std]
 
+extern crate alloc;
+
+use alloc::vec;
+use alloc::vec::Vec;
 use cipher::array::Array;
 use cipher::consts::{U1, U16, U44};
 use cipher::inout::InOut;
 
-use cipher::{AlgorithmName, ArraySize, Block, BlockBackend, BlockCipher, BlockCipherDecrypt, BlockCipherEncrypt, BlockClosure, BlockSizeUser, Key, KeyInit, KeySizeUser, ParBlocksSizeUser, Unsigned};
+use cipher::{
+    AlgorithmName, ArraySize, Block, BlockBackend, BlockCipher, BlockCipherDecrypt,
+    BlockCipherEncrypt, BlockClosure, BlockSizeUser, Key, KeyInit, KeySizeUser, ParBlocksSizeUser,
+    Unsigned,
+};
 use core::cmp::max;
 use core::fmt::Formatter;
+use core::marker::PhantomData;
 
 use core::ops::{BitAnd, BitXor};
 
@@ -16,42 +25,43 @@ use core::ops::{BitAnd, BitXor};
 // B - key length in bytes - 16
 
 // word size in bits
-const W: usize = 32;
 const LG_W: u32 = 5;
-
 
 // number of rounds
 const R: usize = 20;
 
-
-pub struct RC6 {
+pub struct RC6<B: ArraySize> {
     key: Array<u32, U44>,
+    key_size: PhantomData<B>,
 }
 
-impl BlockCipher for RC6 {}
+impl<B: ArraySize> BlockCipher for RC6<B> {}
 
-impl KeySizeUser for RC6 {
-    type KeySize = U16;
+impl<B: ArraySize> KeySizeUser for RC6<B> {
+    type KeySize = B;
 }
 
-impl BlockSizeUser for RC6 {
+impl<B: ArraySize> BlockSizeUser for RC6<B> {
     type BlockSize = U16;
 }
-impl KeyInit for RC6 {
+impl<B: ArraySize> KeyInit for RC6<B> {
     #[inline]
     fn new(key: &Key<Self>) -> Self {
         let expanded_key = key_expansion(key);
-        Self { key: expanded_key }
+        Self {
+            key: expanded_key,
+            key_size: PhantomData,
+        }
     }
 }
 
-impl AlgorithmName for RC6 {
+impl<B: ArraySize> AlgorithmName for RC6<B> {
     fn write_alg_name(f: &mut Formatter<'_>) -> core::fmt::Result {
-        write!(f, "RC6 32/20/16")
+        write!(f, "RC6 32/20/{}", B::U8)
     }
 }
 
-impl BlockCipherEncrypt for RC6 {
+impl<B: ArraySize> BlockCipherEncrypt for RC6<B> {
     fn encrypt_with_backend(&self, f: impl BlockClosure<BlockSize = Self::BlockSize>) {
         f.call(&mut RC6EncBackend {
             expanded_key: self.key,
@@ -101,8 +111,8 @@ impl BlockBackend for RC6EncBackend {
             c = d;
             d = ta;
         }
-        a = a.wrapping_add(self.expanded_key[2*R+2]);
-        c = c.wrapping_add(self.expanded_key[2*R+3]);
+        a = a.wrapping_add(self.expanded_key[2 * R + 2]);
+        c = c.wrapping_add(self.expanded_key[2 * R + 3]);
 
         block.get_out()[0..4].copy_from_slice(&a.to_le_bytes());
         block.get_out()[4..8].copy_from_slice(&b.to_le_bytes());
@@ -111,7 +121,7 @@ impl BlockBackend for RC6EncBackend {
     }
 }
 
-impl BlockCipherDecrypt for RC6 {
+impl<B: ArraySize> BlockCipherDecrypt for RC6<B> {
     fn decrypt_with_backend(&self, f: impl BlockClosure<BlockSize = Self::BlockSize>) {
         f.call(&mut RC6DecBackend {
             expanded_key: self.key,
@@ -138,8 +148,8 @@ impl BlockBackend for RC6DecBackend {
         let mut c = u32::from_le_bytes(block.get_in()[8..12].try_into().unwrap());
         let mut d = u32::from_le_bytes(block.get_in()[12..16].try_into().unwrap());
 
-        c = c.wrapping_sub(self.expanded_key[2*R+3]);
-        a = a.wrapping_sub(self.expanded_key[2*R+2]);
+        c = c.wrapping_sub(self.expanded_key[2 * R + 3]);
+        a = a.wrapping_sub(self.expanded_key[2 * R + 2]);
 
         for i in (1..=R).rev() {
             {
@@ -179,22 +189,20 @@ impl BlockBackend for RC6DecBackend {
 
 type Word = u32;
 fn key_expansion<B: ArraySize>(key: &Array<u8, B>) -> Array<Word, U44> {
-
     // output size only depends on the number of rounds
-    assert_eq!(2*R+4, U44::to_usize());
+    assert_eq!(2 * R + 4, U44::to_usize());
     const P32: Word = 0xB7E15163;
     const Q32: Word = 0x9E3779B9;
 
     // b bytes into c words
-    const C: usize = 4;
-    assert_eq!(C, (B::to_usize() * u8::BITS as usize) / Word::BITS as usize);
+    let bytes_per_word = Word::BITS as usize / 8;
+    let c = (B::to_usize() + bytes_per_word - 1) / bytes_per_word;
 
-    let b = 16;
-    let u = W / 8;
+    let b = B::to_usize();
 
-    let mut l: [Word; C] = [0u32; C];
+    let mut l: Vec<Word> = vec![0; c];
     for i in (0..=(b - 1)).rev() {
-        l[i / u] = (l[i / u] << 8) | (key[i] as u32);
+        l[i / bytes_per_word] = (l[i / bytes_per_word] << 8) | (key[i] as u32);
     }
 
     let mut s: Array<Word, U44> = Array::from_fn(|_| 0u32);
@@ -209,14 +217,14 @@ fn key_expansion<B: ArraySize>(key: &Array<u8, B>) -> Array<Word, U44> {
     let mut i = 0;
     let mut j = 0;
 
-    let v = 3 * max(C, 2*R+4);
+    let v = 3 * max(c, 2 * R + 4);
     for _s in 1..=v {
         a = s[i].wrapping_add(a).wrapping_add(b).rotate_left(3);
         s[i] = a;
         b = (l[j].wrapping_add(a).wrapping_add(b)).rotate_left(a.wrapping_add(b).bitand(0b11111));
         l[j] = b;
-        i = (i + 1) % (2*R+4);
-        j = (j + 1) % C;
+        i = (i + 1) % (2 * R + 4);
+        j = (j + 1) % c;
     }
 
     s
