9b23a6fe549cfeeeec007cec80d0fc3d72a6cdb1
Brian Yarr <brian.yarr@gmail.com>
2024-03-09 19:46:12 +0000

    [WIP] Parameterise enc and ec by Word
    Still some hardcoded nonesense in there.

--- rc6/src/lib.rs
+++ rc6/src/lib.rs
@@ -25,27 +25,25 @@ use core::ops::{BitAnd, BitOr, BitXor, Shl};
 // R - number of rounds - 20
 // B - key length in bytes - 16
 
-// word size in bits
-const LG_W: u32 = 5;
 
 // number of rounds
 const R: usize = 20;
 
-pub struct RC6<B: ArraySize> {
-    key: Array<u32, U44>,
+pub struct RC6<W:Word, B: ArraySize> {
+    key: Array<W, U44>,
     key_size: PhantomData<B>,
 }
 
-impl<B: ArraySize> BlockCipher for RC6<B> {}
+impl<W:Word, B: ArraySize> BlockCipher for RC6<W, B> {}
 
-impl<B: ArraySize> KeySizeUser for RC6<B> {
+impl<W:Word, B: ArraySize> KeySizeUser for RC6<W, B> {
     type KeySize = B;
 }
 
-impl<B: ArraySize> BlockSizeUser for RC6<B> {
-    type BlockSize = U16;
+impl<W:Word, B: ArraySize> BlockSizeUser for RC6<W, B> {
+    type BlockSize = U16; // TODO
 }
-impl<B: ArraySize> KeyInit for RC6<B> {
+impl<W:Word, B: ArraySize> KeyInit for RC6<W, B> {
     #[inline]
     fn new(key: &Key<Self>) -> Self {
         let expanded_key = key_expansion(key);
@@ -56,13 +54,13 @@ impl<B: ArraySize> KeyInit for RC6<B> {
     }
 }
 
-impl<B: ArraySize> AlgorithmName for RC6<B> {
+impl<W: Word, B: ArraySize> AlgorithmName for RC6<W, B> {
     fn write_alg_name(f: &mut Formatter<'_>) -> core::fmt::Result {
-        write!(f, "RC6 32/20/{}", B::U8)
+        write!(f, "RC6 {}/20/{}", W::Bytes::to_u8(), B::U8)
     }
 }
 
-impl<B: ArraySize> BlockCipherEncrypt for RC6<B> {
+impl<W: Word, B: ArraySize> BlockCipherEncrypt for RC6<W, B> {
     fn encrypt_with_backend(&self, f: impl BlockClosure<BlockSize = Self::BlockSize>) {
         f.call(&mut RC6EncBackend {
             expanded_key: self.key,
@@ -70,40 +68,40 @@ impl<B: ArraySize> BlockCipherEncrypt for RC6<B> {
     }
 }
 
-struct RC6EncBackend {
-    expanded_key: Array<u32, U44>,
+struct RC6EncBackend<W: Word> {
+    expanded_key: Array<W, U44>,
 }
 
-impl ParBlocksSizeUser for RC6EncBackend {
+impl <W: Word> ParBlocksSizeUser for RC6EncBackend<W> {
     type ParBlocksSize = U1;
 }
 
-impl BlockSizeUser for RC6EncBackend {
-    type BlockSize = U16;
+impl <W: Word> BlockSizeUser for RC6EncBackend<W> {
+    type BlockSize = U16; // TODO
 }
 
-impl BlockBackend for RC6EncBackend {
+impl <W: Word> BlockBackend for RC6EncBackend<W> {
     fn proc_block(&mut self, mut block: InOut<'_, '_, Block<Self>>) {
-        let mut a = u32::from_le_bytes(block.get_in()[..4].try_into().unwrap());
-        let mut b = u32::from_le_bytes(block.get_in()[4..8].try_into().unwrap());
-        let mut c = u32::from_le_bytes(block.get_in()[8..12].try_into().unwrap());
-        let mut d = u32::from_le_bytes(block.get_in()[12..16].try_into().unwrap());
+        let mut a = W::from_le_bytes(block.get_in()[..4].try_into().unwrap());
+        let mut b = W::from_le_bytes(block.get_in()[4..8].try_into().unwrap());
+        let mut c = W::from_le_bytes(block.get_in()[8..12].try_into().unwrap());
+        let mut d = W::from_le_bytes(block.get_in()[12..16].try_into().unwrap());
 
         b = b.wrapping_add(self.expanded_key[0]);
         d = d.wrapping_add(self.expanded_key[1]);
         for i in 1..=R {
             let t = b
-                .wrapping_mul(b.wrapping_mul(2).wrapping_add(1))
-                .rotate_left(LG_W);
+                .wrapping_mul(b.wrapping_mul(2.into()).wrapping_add(1.into()))
+                .rotate_left(W::LG_W);
             let u = d
-                .wrapping_mul(d.wrapping_mul(2).wrapping_add(1))
-                .rotate_left(LG_W);
+                .wrapping_mul(d.wrapping_mul(2.into()).wrapping_add(1.into()))
+                .rotate_left(W::LG_W);
 
             a = (a.bitxor(t))
-                .rotate_left(u.bitand(0b11111))
+                .rotate_left(u.bitand(0b11111.into()))
                 .wrapping_add(self.expanded_key[2 * i]);
             c = (c.bitxor(u))
-                .rotate_left(t.bitand(0b11111))
+                .rotate_left(t.bitand(0b11111.into()))
                 .wrapping_add(self.expanded_key[2 * i + 1]);
 
             let ta = a;
@@ -122,7 +120,7 @@ impl BlockBackend for RC6EncBackend {
     }
 }
 
-impl<B: ArraySize> BlockCipherDecrypt for RC6<B> {
+impl<W: Word, B: ArraySize> BlockCipherDecrypt for RC6<W, B> {
     fn decrypt_with_backend(&self, f: impl BlockClosure<BlockSize = Self::BlockSize>) {
         f.call(&mut RC6DecBackend {
             expanded_key: self.key,
@@ -130,24 +128,24 @@ impl<B: ArraySize> BlockCipherDecrypt for RC6<B> {
     }
 }
 
-struct RC6DecBackend {
-    expanded_key: Array<u32, U44>,
+struct RC6DecBackend<W: Word> {
+    expanded_key: Array<W, U44>,
 }
 
-impl ParBlocksSizeUser for RC6DecBackend {
+impl <W: Word> ParBlocksSizeUser for RC6DecBackend<W> {
     type ParBlocksSize = U1;
 }
 
-impl BlockSizeUser for RC6DecBackend {
+impl <W: Word> BlockSizeUser for RC6DecBackend<W> {
     type BlockSize = U16;
 }
 
-impl BlockBackend for RC6DecBackend {
+impl <W: Word> BlockBackend for RC6DecBackend<W> {
     fn proc_block(&mut self, mut block: InOut<'_, '_, Block<Self>>) {
-        let mut a = u32::from_le_bytes(block.get_in()[..4].try_into().unwrap());
-        let mut b = u32::from_le_bytes(block.get_in()[4..8].try_into().unwrap());
-        let mut c = u32::from_le_bytes(block.get_in()[8..12].try_into().unwrap());
-        let mut d = u32::from_le_bytes(block.get_in()[12..16].try_into().unwrap());
+        let mut a = W::from_le_bytes(block.get_in()[..4].try_into().unwrap());
+        let mut b = W::from_le_bytes(block.get_in()[4..8].try_into().unwrap());
+        let mut c = W::from_le_bytes(block.get_in()[8..12].try_into().unwrap());
+        let mut d = W::from_le_bytes(block.get_in()[12..16].try_into().unwrap());
 
         c = c.wrapping_sub(self.expanded_key[2 * R + 3]);
         a = a.wrapping_sub(self.expanded_key[2 * R + 2]);
@@ -162,19 +160,19 @@ impl BlockBackend for RC6DecBackend {
             }
 
             let u = d
-                .wrapping_mul(d.wrapping_mul(2).wrapping_add(1))
-                .rotate_left(LG_W);
+                .wrapping_mul(d.wrapping_mul(2.into()).wrapping_add(1.into()))
+                .rotate_left(W::LG_W);
             let t = b
-                .wrapping_mul(b.wrapping_mul(2).wrapping_add(1))
-                .rotate_left(LG_W);
+                .wrapping_mul(b.wrapping_mul(2.into()).wrapping_add(1.into()))
+                .rotate_left(W::LG_W);
 
             c = c
                 .wrapping_sub(self.expanded_key[2 * i + 1])
-                .rotate_right(t & 0b11111)
+                .rotate_right(t.bitand(0b11111.into()))
                 .bitxor(u);
             a = a
                 .wrapping_sub(self.expanded_key[2 * i])
-                .rotate_right(u & 0b11111)
+                .rotate_right(u.bitand(0b11111.into()))
                 .bitxor(t);
         }
 
@@ -192,6 +190,7 @@ trait Word:
     Shl<Output = Self>
     + From<u8>
     + BitOr<Self, Output = Self>
+    + BitXor<Self, Output=Self>
     + BitAnd<Self, Output = Self>
     + DefaultIsZeroes
 {
@@ -201,18 +200,22 @@ trait Word:
 
     const BITS: u32;
 
+    const LG_W: Self;
+
     fn wrapping_add(self, rhs: Self) -> Self;
+    fn wrapping_sub(self, rhs: Self) -> Self;
 
     fn wrapping_mul(self, rhs: Self) -> Self;
 
     fn rotate_left(self, rhs: Self) -> Self;
+    fn rotate_right(self, rhs: Self) -> Self;
 
     fn from_le_bytes(bytes: &Array<u8, Self::Bytes>) -> Self;
     fn to_le_bytes(self) -> Array<u8, Self::Bytes>;
 }
 
 macro_rules! impl_word_for_primitive {
-    ($primitive:ident, $bytes:ty, $P:expr, $Q:expr) => {
+    ($primitive:ident, $bytes:ty, $lgw:expr, $P:expr, $Q:expr) => {
         impl Word for $primitive {
             type Bytes = $bytes;
             const P: Self = $P;
@@ -220,8 +223,15 @@ macro_rules! impl_word_for_primitive {
 
             const BITS: u32 = $primitive::BITS;
 
+            const LG_W: Self = $lgw;
+
             fn wrapping_add(self, rhs: Self) -> Self {
                 self.wrapping_add(rhs)
+            }
+
+
+            fn wrapping_sub(self, rhs: Self) -> Self {
+                self.wrapping_sub(rhs)
             }
 
             fn wrapping_mul(self, rhs: Self) -> Self {
@@ -230,6 +240,10 @@ macro_rules! impl_word_for_primitive {
 
             fn rotate_left(self, rhs: Self) -> Self {
                 self.rotate_left(rhs)
+            }
+
+            fn rotate_right(self, rhs: Self) -> Self {
+                self.rotate_right(rhs)
             }
 
             fn from_le_bytes(bytes: &Array<u8, Self::Bytes>) -> Self {
@@ -244,7 +258,7 @@ macro_rules! impl_word_for_primitive {
     };
 }
 
-impl_word_for_primitive!(u32, U4, 0xB7E15163, 0x9E3779B9);
+impl_word_for_primitive!(u32, U4, 5, 0xB7E15163, 0x9E3779B9);
 
 fn key_expansion<W: Word, B: ArraySize>(key: &Array<u8, B>) -> Array<W, U44> {
     // output size only depends on the number of rounds
