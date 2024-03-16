8ff151ef7d57d6b955d2c487436e4420fc0a312a
Brian Yarr <brian.yarr@gmail.com>
2024-03-16 10:34:27 +0000

    Fix bench marks

--- rc6/benches/mod.rs
+++ rc6/benches/mod.rs
@@ -3,29 +3,29 @@ extern crate test;
 
 use cipher::block_decryptor_bench;
 use cipher::block_encryptor_bench;
-use cipher::consts::{U16, U32};
+use cipher::consts::{U16, U20, U32};
 use rc6::RC6;
 
 block_encryptor_bench!(
-    Key: RC6<U16>,
+    Key: RC6<u32, U20, U16>,
     rc6_32_20_16_encrypt_block,
     rc6_32_20_16_encrypt_blocks,
 );
 
 block_decryptor_bench!(
-    Key: RC6<U16>,
+    Key: RC6<u32, U20, U16>,
     rc6_32_20_16_decrypt_block,
     rc6_32_20_16_decrypt_blocks,
 );
 
 block_encryptor_bench!(
-    Key: RC6<U32>,
+    Key: RC6<u32, U20, U32>,
     rc6_32_20_32_encrypt_block,
     rc6_32_20_32_encrypt_blocks,
 );
 
 block_decryptor_bench!(
-    Key: RC6<U32>,
+    Key: RC6<u32, U20, U32>,
     rc6_32_20_32_decrypt_block,
     rc6_32_20_32_decrypt_blocks,
 );
